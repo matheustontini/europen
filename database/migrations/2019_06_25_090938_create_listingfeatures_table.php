2d51a0898181b3fd4310804d6106e33ef3614cc9
Matheus Tontini <tontini92@gmail.com>
2019-06-25 13:15:00 +0200

    Migrations

---
+++ database/migrations/2019_06_25_090938_create_listingfeatures_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateListingFeaturesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ListingFeatures', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('user_id');
+            $table->unsignedDecimal('saleprice')->nullable;
+            $table->unsignedDecimal('rentalprice')->nullable;
+            $table->unsignedDecimal('totalsquaremeters')->nullable;
+            $table->unsignedInteger('rooms')->nullable;
+            $table->unsignedDecimal('bathrooms')->nullable;
+            $table->unsignedInteger('yearbuilt')->nullable;
+            $table->string('countryname');
+            $table->string('cityname');
+            $table->string('zipcode')->nullable;
+            $table->string('mailaddress')->nullable;
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('listingfeatures');
+    }
+}
