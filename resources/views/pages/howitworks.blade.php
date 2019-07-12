df866283736723942892eb32fba51dfbcc21f0f5
Daniela Vazquez <daniela_056@hotmail.com>
2019-07-12 09:55:10 -0500

    how it works title and subtitles

--- resources/views/pages/howitworks.blade.php
+++ resources/views/pages/howitworks.blade.php
@@ -1,5 +1,147 @@
 @extends('layout')
 
 @section('content')
-    here
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 py-5 justify-content-center">
+                <div class="background-how-it hover">
+                    <h1 class="title display-3">The modern way to buy or sell.</h1>
+                    <h1 class="title display-4">We make buying or renting your home exciting again.</h1>
+                    <h1 class="title display-4">Get started by learning how it works.</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-6 copy">
+                <h2 class="subtitle">Who are we?</h2>
+                <p class="text">An online European real estate brokerage focused exclusively on representing buyers/renters. 
+                        This is done by facilitating a closed deal by using eurOpen's platform to  create/view hundreds of listings and enabling our clients to tour any of them by accessing our hypothetical key boxes through a code sent directly on our app.</p>
+            </div>
+            <div class="col-6 copy">
+                    <h2 class="subtitle">Buying from eurOpen</h2>
+                        <p class="text">We want to make buying or renting a home/flat as exciting as it sounds! 
+                        Whether you’re buying or renting directly from us or with an agent, you can view hundreds of listings in the eurozone.
+                        Not everyone wants to spend their entire weekend at viewings so we’ve made it easy to tour any of our listings any day, no appointment needed. </p>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="container">
+         <div class="row">
+            <div class="col-12">
+                <h2 class="title"> Sell with eurOpen</h2>
+            </div>
+                <div class="col-1 number">
+                    <p>1</p>
+                </div>
+            <div class="col-11 copy">
+                <h3>Request a Free Offer</h3>
+                    <p class="text">Enter your home information such as address and home feautres. We ask if you have things such as hardwood floors, A/C, or granite countertops. It helps to have information such as year home was built and square footage.</p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-1 number">
+                <p>2</p>
+            </div>
+            <div class="col-11 copy">
+                <h3>We'll prepare a competitive offer</h3>
+                    <p class="text">If you and your hom qualify, our eurOpen local real estate experts will begin to prepare an offer.</p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-1 number">
+                <p>3</p>
+            </div>
+            <div class="col-11 copy">
+                <h3>Complete in-person home assesment</h3>
+                    <p class="text">If you and your home qualify, our eurOpen local real estate experts will begin to prepare an offer.</p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-1 number">
+                <p>4</p>
+            </div>
+            <div class="col-11 copy">
+                <h3>Close and Cash-in</h3>
+                    <p class="text">You chose the most convenient close date so you can plan your move at a comfortable pace. After moving, you get paid in 2 business days. You can then focus on celebrating your quick and </p>
+            </div>
+        </div>
+    </div>
+
+
+
+    {{-- HERE --}}
+    <div class="container">
+            <div class="row">
+               <div class="col-12">
+                   <h2 class="title"> Buy with eurOpen</h2>
+               </div>
+                   <div class="col-1 number">
+                       <p>1</p>
+                   </div>
+               <div class="col-11 copy">
+                   <h3>some text</h3>
+                       <p class="text">text here</p>
+               </div>
+           </div>
+   
+           <div class="row">
+               <div class="col-1 number">
+                   <p>2</p>
+               </div>
+               <div class="col-11 copy">
+                   <h3>some text</h3>
+                       <p class="text">text here</p>
+               </div>
+           </div>
+   
+           <div class="row">
+               <div class="col-1 number">
+                   <p>3</p>
+               </div>
+               <div class="col-11 copy">
+                   <h3>some text</h3>
+                       <p class="text">text here</p>
+               </div>
+           </div>
+   
+           <div class="row">
+               <div class="col-1 number">
+                   <p>4</p>
+               </div>
+               <div class="col-11 copy">
+                   <h3>some text</h3>
+                       <p class="text">text here</p>
+               </div>
+           </div>
+
+           <div class="row">
+                <div class="col-1 number">
+                    <p>5</p>
+                </div>
+                <div class="col-11 copy">
+                    <h3>some text</h3>
+                        <p class="text">text here</p>
+                </div>
+            </div>
+
+            <div class="row">
+                    <div class="col-1 number">
+                        <p>6</p>
+                    </div>
+                    <div class="col-11 copy">
+                        <h3>some text</h3>
+                            <p class="text">text here</p>
+                    </div>
+                </div>
+       </div>
+    
+
+
 @endsection
