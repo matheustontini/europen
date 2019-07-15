1c51bca1ec8e3c0329066ab844d2e0922c393380
Daniela Vazquez <daniela_056@hotmail.com>
2019-07-15 09:33:41 -0500

    Finished how it works page

--- resources/views/pages/howitworks.blade.php
+++ resources/views/pages/howitworks.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container-fluid">
         <div class="row">
-            <div class="col-12 py-5 justify-content-center">
+            <div class="col-12 justify-content-center">
                 <div class="background-how-it hover">
                     <h1 class="title display-3">The modern way to buy or sell.</h1>
                     <h1 class="title display-4">We make buying or renting your home exciting again.</h1>
@@ -48,7 +48,7 @@
                 <p>2</p>
             </div>
             <div class="col-11 copy">
-                <h3>We'll prepare a competitive offer</h3>
+                <h3>We'll Prepare a Competitive Offer</h3>
                     <p class="text">If you and your hom qualify, our eurOpen local real estate experts will begin to prepare an offer.</p>
             </div>
         </div>
@@ -58,7 +58,7 @@
                 <p>3</p>
             </div>
             <div class="col-11 copy">
-                <h3>Complete in-person home assesment</h3>
+                <h3>Complete in-person Home Assesment</h3>
                     <p class="text">If you and your home qualify, our eurOpen local real estate experts will begin to prepare an offer.</p>
             </div>
         </div>
@@ -76,7 +76,7 @@
 
 
 
-    {{-- HERE --}}
+    
     <div class="container">
             <div class="row">
                <div class="col-12">
@@ -86,8 +86,10 @@
                        <p>1</p>
                    </div>
                <div class="col-11 copy">
-                   <h3>some text</h3>
-                       <p class="text">text here</p>
+                   <h3>Browse Homes Using eurOpen App</h3>
+                       <p class="text">Visit our eurOpen owned homes and unlock them from 6AM to 9PM at your convenience.
+                           In certain cities you can even schedule free, on-demand tours with any of our agents.
+                       </p>
                </div>
            </div>
    
@@ -96,8 +98,10 @@
                    <p>2</p>
                </div>
                <div class="col-11 copy">
-                   <h3>some text</h3>
-                       <p class="text">text here</p>
+                   <h3>Want to Tour?</h3>
+                       <p class="text">If you are in the area of the listing you want to visit, simply unlock any of our key-boxes with a randomly generated security code. <br>
+                        You can check-out the listing for a maximum of 2 hours and see the property in person.
+                       </p>
                </div>
            </div>
    
@@ -106,8 +110,8 @@
                    <p>3</p>
                </div>
                <div class="col-11 copy">
-                   <h3>some text</h3>
-                       <p class="text">text here</p>
+                   <h3>Complete Quick Background Check</h3>
+                       <p class="text">In order to take advantage of eurOpen's exceptional services, sign up and complete our quick background-check questionnaire.<br> Be sure to have information such as employment, credit, and housing history.</p>
                </div>
            </div>
    
@@ -116,8 +120,9 @@
                    <p>4</p>
                </div>
                <div class="col-11 copy">
-                   <h3>some text</h3>
-                       <p class="text">text here</p>
+                   <h3>Make an Offer</h3>
+                       <p class="text">Found your perfect home? Start an offer online or right from the eurOpen app. <br>
+                    We have experienced eurOpen home agents that can guide you through the process of purchase.</p>
                </div>
            </div>
 
@@ -126,21 +131,19 @@
                     <p>5</p>
                 </div>
                 <div class="col-11 copy">
-                    <h3>some text</h3>
-                        <p class="text">text here</p>
+                    <h3>Close and Move-in</h3>
+                        <p class="text">You choose your close and move-in date!  In eastern european markets you may even have the chance of getting 1% back when you work with one of eurOpen's agents.</p>
                 </div>
             </div>
+        </div>
 
-            <div class="row">
-                    <div class="col-1 number">
-                        <p>6</p>
-                    </div>
-                    <div class="col-11 copy">
-                        <h3>some text</h3>
-                            <p class="text">text here</p>
-                    </div>
-                </div>
-       </div>
+    <div class="links">
+        <h2 class='display'>Learn More</h2>
+        <a href="https://www.opendoor.com/w/guides/what-is-an-ibuyer" target="_blank">What is an iBuyer and How Does it Work?</a><br>
+        <a href="https://moneyinc.com/the-top-five-ibuyers-in-the-real-estate-industry/" target="_blank">Top 5 buyers in the Real Estate Industry</a><br>
+        <a href="https://hooquest.com/top-ibuyers-list/" target="_blank">The Ultimate Guide to the Real Estate “iBuyers” in 2019</a><br>
+    </div>
+    
     
 
 
