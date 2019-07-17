bf5750bdeed8abbe1972a1646a1d481b2f1de4f7
Daniela Vazquez <daniela_056@hotmail.com>
2019-07-17 04:20:23 -0500

    Adding an appointment page

---
+++ resources/views/pages/appointment.blade.php
@@ -0,0 +1,58 @@
+@extends('layout')
+
+@section('content')
+<div class="container-fluid">
+    <h1>Appointment Scheduler</h1>
+    <h2>Book and Appointment to see a listing right from the touch of a button</h2>
+    <div class="row">
+        <div class="col-12">
+
+        </div>
+    </div>
+    
+    <form>
+        <div class="form-group">
+          <label for="">Email address</label>
+          <input type="email" class="form-control" id="" placeholder="">
+        </div>
+        <div class="form-group">
+          <label for="">Example select</label>
+          <select class="form-control" id="">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </select>
+        </div>
+        <div class="form-group">
+          <label for="">Example multiple select</label>
+          <select multiple class="form-control" id="">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </select>
+        </div>
+        <div class="form-group">
+          <label for="">Example textarea</label>
+          <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>
+      </form>
+   
+
+</div>
+
+
+
+
+
+ 
+    
+    
+
+
+@endsection
+</body>
+</html>
